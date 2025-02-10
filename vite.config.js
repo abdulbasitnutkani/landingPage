c15fa6964c6c44f4859dd83c44bd307e2d0371a0
abdulbasitnutkani <abdulbasit@codebotx.com>
2025-02-10 17:44:32 +0500

    update the config.yml file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    fs: {
+      strict: false, // Allow serving files outside of src
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html", // Main app entry point
+        admin: "public/admin/index.html", // Decap CMS entry point
+      },
+    },
+  },
 });
